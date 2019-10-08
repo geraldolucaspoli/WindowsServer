7d91b9ebe429175bbc7b72aec116d87a63ecfa98
geraldolucaspoli <locaspolix3@gmail.com>
2019-10-08 17:23:21 -0300

    Delete install_dotnet_3.5_online.ps1

--- install_dotnet_3.5_online.ps1
+++
@@ -1,3 +0,0 @@
-cd C:\Windows\Temp
-Invoke-WebRequest -UseBasicParsing https://github.com/geraldolucaspoli/WindowsServer/blob/master/microsoft-windows-netfx3-ondemand-package.cab  -OutFile microsoft-windows-netfx3-ondemand-package.cab
-Dism /online /enable-feature /featurename:NetFX3 /All /Source:C:\Windows\Temp /LimitAccess
